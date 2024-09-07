602238c61486ff36224fd24d448fe84d72793f40
RouQandel <rawanqandel62@gmail.com>
2024-09-07 21:17:49 +0300

    Add SocialMediaMvc

--- DataAccess Layer_Database/obj/Release/net8.0/DataAccess Layer_Database.AssemblyInfo.cs
+++ DataAccess Layer_Database/obj/Release/net8.0/DataAccess Layer_Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess Layer_Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c1ddc7743700a246595b3da6a0219eee71b5b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess Layer_Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess Layer_Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
