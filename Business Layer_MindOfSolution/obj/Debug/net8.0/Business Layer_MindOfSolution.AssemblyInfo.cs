a235ea8688e1d0e728b5ffd9475bc5c54bf944d6
RouQandel <rawanqandel62@gmail.com>
2024-09-07 21:18:40 +0300

    add

--- Business Layer_MindOfSolution/obj/Debug/net8.0/Business Layer_MindOfSolution.AssemblyInfo.cs
+++ Business Layer_MindOfSolution/obj/Debug/net8.0/Business Layer_MindOfSolution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business Layer_MindOfSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c1ddc7743700a246595b3da6a0219eee71b5b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business Layer_MindOfSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business Layer_MindOfSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
